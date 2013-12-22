29e2293eab1e8586ee15d22d008795280151772f
saxap <saxap@bk.ru>
2013-12-22 21:16:56 +0400

    Fix bugs with select lists

--- jquery.grab-gets.js
+++ jquery.grab-gets.js
@@ -1,10 +1,9 @@
 /*!
  * jQuery grabget plugin
  * Description: Grab GET parameters from url and put, select and check elements of selected form
- * version: 1.0.1-2013.11.10
+ * version: 1.0.4-2013.22.12
  * Requires jQuery v1.2 or later
  * Autor: saxa:p (http://dontforget.pro)
- * Email: saxap@bk.ru
  * Examples and documentation at: http://malsup.com/jquery/form/
  * Project repository: https://github.com/malsup/form
  * Dual licensed under the MIT and GPL licenses.
@@ -18,16 +17,17 @@
     $.extend({
       getUrlVars: function(re){
         var vars = [], hash;
+        var h0 = '';
         var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
         for(var i = 0; i < hashes.length; i++)
         {
           hash = hashes[i].split('=');
+          h0 = hash[0];
           if (!re) {
           vars.push(hash[0]);
           vars[hash[0]] = hash[1];
-          } else {
-          vars.push(hash[1]);
-          vars[hash[1]] = hash[0];
+          } else {            
+          vars[decodeURIComponent(hash[1])] = decodeURIComponent(h0);
           }
         }
         return vars;
@@ -70,14 +70,16 @@
         form.find('select').each( function() {
           var iname = $(this).attr('name');
           $(this).find('option').each( function() {
-            if ( $.inArray($(this).val(), arr) !== -1 ) {
+           var ival = $(this).val();
+            if ( arr[ival] == iname ) {
               $(this).attr('selected', 'selected');
             }
           });
         });
         form.find('input[type="checkbox"], input[type="radio"]').each( function() {
           var iname = $(this).attr('name');
-          if ( $.inArray($(this).val(), arr) !== -1 ) {
+          var ival = $(this).val();
+          if ( arr[ival] == iname ) {
             $(this).attr('checked', 'checked');
           }
         });
